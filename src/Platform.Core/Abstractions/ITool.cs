8831efe917d5560fc603b1a960e6b3876fcf7166
ahmadradwan0 <ahmad@ahmadradwan.dev>
2026-06-06 09:31:41 -0400

    cc 2

---
+++ src/Platform.Core/Abstractions/ITool.cs
@@ -0,0 +1,9 @@
+﻿namespace Platform.Core.Abstractions
+{
+    public interface ITool
+    {
+        string Name { get; }
+        string Description { get; }
+        IEnumerable<ICommandModule> CommandModules { get; }
+    }
+}
---
+++ src/Platform.Tools/FileSync/FileSyncTool.cs
@@ -0,0 +1,11 @@
+﻿using Platform.Core.Abstractions;
+using Platform.Tools.FileSync.CommandModules;
+namespace Platform.Tools.FileSync
+{
+    public class FileSyncTool(FileSyncCommandModule fileSyncCommandModule) : ITool
+    {
+        public string Name => "sync";
+        public string Description => "File synchronization tools";
+        public IEnumerable<ICommandModule> CommandModules => [fileSyncCommandModule];
+    }
+}

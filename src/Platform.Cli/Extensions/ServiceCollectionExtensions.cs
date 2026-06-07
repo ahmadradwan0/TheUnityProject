e5d343ae6e52a81e761a693de2417001b3c07982
ahmadradwan0 <ahmad@ahmadradwan.dev>
2026-06-07 11:18:19 -0400

    init

---
+++ src/Platform.Cli/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Platform.Core.Abstractions;
+using Platform.Infrastructure.FileSystem;
+using Platform.Infrastructure.Logging;
+using Platform.Infrastructure.Processes;
+using Platform.Tools.FileSync;
+using Platform.Tools.FileSync.CommandModules;
+using Platform.Tools.FileSync.LocalSync;
+using Platform.Tools.FileSync.Services;
+
+namespace Platform.Cli.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<IFileSystem, FileSystem>();
+            services.AddSingleton<IAppLogger, AppLogger>();
+            services.AddSingleton<IProcessRunner, ProcessRunner>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddFileSyncTool(this IServiceCollection services)
+        {
+            services.AddSingleton<ISyncStrategy, LocalSync>();
+            services.AddSingleton<ITool, FileSyncTool>();
+            services.AddSingleton<FileSyncService>();
+            services.AddSingleton<FileSyncCommandModule>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Platform.Cli/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Platform.Cli.Extensions;
+using Platform.Core.Abstractions;
+using System.CommandLine;
+
+ServiceCollection services = new();
+services.AddInfrastructure();
+services.AddFileSyncTool();
+
+ServiceProvider provider = services.BuildServiceProvider();
+
+IEnumerable<ITool> tools = provider.GetServices<ITool>();
+
+RootCommand rootCommand = new("Unity CLI Toolkit");
+
+foreach (ITool tool in tools)
+{
+    foreach (ICommandModule module in tool.CommandModules)
+    {
+        rootCommand.Add(module.Build());
+    }
+}
+
+rootCommand.Parse(args).Invoke();
